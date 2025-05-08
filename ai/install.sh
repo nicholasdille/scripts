4073dfb4b1cd6ba33c632be9d95e52304bafa96c
Nicholas Dille <nicholas.dille@mailbox.org>
2025-05-08 23:04:44 +0200

    Updates

--- ai/install.sh
+++ ai/install.sh
@@ -17,23 +17,4 @@ case "${arch}" in
         ;;
 esac
 
-if ! test -f bin/ollama; then
-    version=0.6.8
-    echo "### Installing Ollama ${version} for ${arch}..."
-    curl -sSLf "https://github.com/ollama/ollama/releases/download/v${version}/ollama-linux-${alt_arch}.tgz" \
-    | tar --extract --gzip --no-same-owner
-fi
-
-docker compose up --detach
-
-# USAGE: OPENAI_ENDPOINT=http://localhost:11434 OPENAI_DEPLOYMENT_NAME=llama3.2 OPENAI_API_KEY="n/a" kubectl-ai
-#version=0.0.13
-#echo "### Installing Kubectl-AI (community) ${version} for ${arch}..."
-#curl -sSLf "https://github.com/sozercan/kubectl-ai/releases/download/v${version}/kubectl-ai_linux_${alt_arch}.tar.gz" \
-#| tar --extract --gzip --no-same-owner
-
-# USAGE: kubectl-ai --llm-provider=ollama --model=llama3.2
-version=0.0.8
-echo "### Installing Kubectl-AI (Google) ${version} for ${arch}..."
-curl -sSLf "https://github.com/GoogleCloudPlatform/kubectl-ai/releases/download/v${version}/kubectl-ai_Linux_${arch_suffix}.tar.gz" \
-| tar --extract --gzip --no-same-owner
+docker compose up --detach ollama
